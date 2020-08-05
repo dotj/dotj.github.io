ae8d0f5daae039e555f0b094ff1718624ae34cf2
julietdo <hellojuliedo@gmail.com>
2020-08-05 16:26:55 -0700

    Remove links

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -4,7 +4,7 @@ const Bio = () => (
   <div>
     <p>Hi I'm Julie. I'm a software developer and UX researcher focusing on tech ethics, privacy, and social psychology. Let's chat about wicked problems. 🧙🏻‍♀</p>
 
-    <p>Contact me at <em>hellojuliedo at gmail dot com</em>, or find me on <a href="https://linkedin.com/in/julietdo">LinkedIn</a> and <a href="https://github.com/dotj">GitHub</a>.</p>
+    <p>Contact me at <em>hellojuliedo at gmail dot com</em>.</p>
   </div>
 )
 
