94dae2d41288e5840bc6f8592d7414b8c03d7992
julietdo <hellojuliedo@gmail.com>
2020-10-07 15:57:21 -0700

    Updates

---
+++ component---src-pages-404-tsx-cedb6f64017c785dd43f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"i6+/":function(e,t,n){"use strict";n.r(t);var u=n("q1tI"),a=n("9Dj+"),l=n("H8eV");t.default=function(){return u.createElement(a.a,null,u.createElement(l.a,{title:"404: Not found"}),u.createElement("h1",null,"NOT FOUND"),u.createElement("p",null,"You just hit a route that doesn't exist. :("))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-cedb6f64017c785dd43f.js.map
