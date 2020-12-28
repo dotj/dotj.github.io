71b20337cc4fbaed8f4563c76fd4476aa580f384
julietdo <hellojuliedo@gmail.com>
2020-12-28 13:20:09 -0800

    lol typo

--- src/components/bio.js
+++ src/components/bio.js
@@ -4,7 +4,7 @@ const Bio = () => {
   return (
     <div>
       <p><h1>Hi, I'm Julie.</h1></p>
-      <p>I'm a software developer with experience in UX reasearch and design and an interest in tech and society, equity, and privacy. Let's chat about wicked problems. <span role="img" aria-label="woman mage">🧙🏻‍♀</span></p>
+      <p>I'm a software developer with experience in UX research and design and an interest in tech and society, equity, and privacy. Let's chat about wicked problems. <span role="img" aria-label="woman mage">🧙🏻‍♀</span></p>
       <p>Contact me at <mark>hellojuliedo at gmail dot com</mark>.</p>
     </div>
   )
